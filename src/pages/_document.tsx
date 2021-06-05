abd22979358637b9dd969d1663658e6c8c81407e
Renan Lima <cursosprogramacoes@gmail.com>
2021-06-05 06:39:25 -0300

    add font externa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (      
       <Html>
-      <Head>        
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
         <title>ig.news</title>
       </Head>
       <body>
